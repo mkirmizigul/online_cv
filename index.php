be292efa1c802abc639f654ccb9820d96601509e
mkirmizigul <muratkirmizigul@gmail.com>
2015-12-07 17:46:29 +0200

    .

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html>
-<head>
-		<meta charset="utf-8">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="tr-TR">
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>jQeury.steps Demos</title>
         <meta name="description" content="">
@@ -9,7 +9,8 @@
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet" href="css/jquery.steps.css">
-        
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-datepicker.min.css">
         <script src="js/modernizr-2.6.2.min.js"></script>
         <script src="js/jquery-1.9.1.min.js"></script>
         <script src="js/jquery.cookie-1.3.1.js"></script>
@@ -18,6 +19,10 @@
         <script src="dist/jquery.validate.js"></script>
         
         <script src="js/custom.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        
+        <script src="js/bootstrap-datepicker.min.js"></script>
+        <script src="locales/bootstrap-datepicker.tr.min.js"></script>
         
 <title>Insert title here</title>
 </head>
@@ -25,14 +30,45 @@
          <script>
                 $(function ()
                 {
+
                 	var form = $("#example-form");
+
+                	                	
                 	form.validate({
-                	    errorPlacement: function errorPlacement(error, element) { element.before(error); },
-                	    rules: {
-                	        confirm: {
-                	            equalTo: "#password"
-                	        }
-                	    }
+                		rules: {
+            				ad: {
+            					required: true
+            					
+            				},
+            				eposta: {
+            					required: true,
+            					email: true,
+            					maxlength: 255
+            					
+            				},
+            				
+            				soyad: {
+            					required: true,
+            					maxlength: 50,
+            				
+            					
+            				}            			
+            			},
+            			messages: {
+            				ad: {
+            					required:'Adınızı giriniz.'
+            				},
+            				soyad: {
+            					required:'Soyadınızı giriniz.'
+            				},
+            				eposta: {
+            					required:'Eposta adresinizi giriniz.',
+            					email:'Eposta adresinizi doğru formatta giriniz.'
+            				},
+            				dogumTarihi: {
+            					required:'Doğum tarihinizi giriniz.'
+            				}
+            			}
                 	});
                 	form.children("div").steps({
                 	    headerTag: "h3",
@@ -50,22 +86,48 @@
                 	    },
                 	    onFinished: function (event, currentIndex)
                 	    {
-                	        alert("Submitted!");
+                	        alert("Tebrikler CV nizi Başarılı Bir Şekilde Oluşturdunuz!");
                 	    }
+                	});
+
+                	$('.date').datepicker({
+                	    format: 'dd/mm/yyyy',
+                	    language:'tr',
+                	    todayHighlight:true
+                	    
+                	    
                 	});
                 });
             </script>
+            <br>
  <form id="example-form" action="#">
-    <div>
-        <h3>Account</h3>
+    <div class="row col-lg-9">
+      
+        <h3>Kişisel Bilgileriniz</h3>
         <section>
-            <label for="userName">User name *</label>
-            <input id="userName" name="userName" type="text" class="required">
-            <label for="password">Password *</label>
-            <input id="password" name="password" type="text" class="required">
-            <label for="confirm">Confirm Password *</label>
-            <input id="confirm" name="confirm" type="text" class="required">
-            <p>(*) Mandatory</p>
+        <div class="form-group">
+            <input placeholder="Adınız" id="ad" name="ad" type="text" class="form-control required">
+        </div>
+        
+        <div class="form-group">
+            <input placeholder="Soyadınız" id="soyad" name="soyad" type="text" class="form-control required">
+        </div>
+        
+        <div class="input-group date" data-provide="datepicker" >
+    		<input type="text" class="form-control required" placeholder="Doğum Tarihiniz gün/ay/yıl" id="dogumTarihi" name="dogumTarihi">
+    			<div class="input-group-addon">
+        		<span class="glyphicon glyphicon-th"></span>
+    		</div>
+		</div>      
+            <br>
+        <div class="form-group">
+            <input placeholder="E-Posta Adresiniz" id="eposta" name="eposta" type="text" class="form-control required">
+        </div>
+            
+        <div class="form-group">
+            <input placeholder="Telefon Numaranız" id="telefon" name="telefon" type="text" class="form-control required">
+        </div>
+
         </section>
         <h3>Profile</h3>
         <section>
@@ -91,6 +153,7 @@
         <section>
             <input id="acceptTerms" name="acceptTerms" type="checkbox" class="required"> <label for="acceptTerms">I agree with the Terms and Conditions.</label>
         </section>
+    
     </div>
 </form>
 </body>
